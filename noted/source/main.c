dc633329592b52eb5d4ed437fa71a3c25f4f8e6d
Lucas Song <lsong013@wch136-16.cs.ucr.edu>
2020-03-12 13:11:58 -0700

    finish 2 display modes

--- noted/source/main.c
+++ noted/source/main.c
@@ -162,10 +162,107 @@ ISR (TIMER1_COMPA_vect) {
 		ADCSRA |= (1<<ADSC);
 	}
 }
+
+unsigned char x;
+char buf[128];
+unsigned l;
+unsigned m;
+unsigned h;
+char allMode[40] = "all bin display \n";
+char low[40] = "low frequency";
+char medium[40] = "med frequency";
+char high[40] = "high frequency";
+char no[40] = "no sound";
+int index = 0;
+int j;
+
+// guess mode tries to guess what frequency
+// all mode displays all bins
+enum states {start, dGuess, dAll, waitUpG, waitUpA} states;
+
+void tick() {
+	unsigned char button = ~PINC & 0x01;
+	switch(states) {
+		case start:
+			states = dAll;
+			break;
+		case dAll:
+			if (button) { states = waitUpG;}
+			//states = dAll;
+			break;
+		case waitUpG:
+			if (!button) { states = dGuess;}
+			//states = waitUpG;
+			break;
+		case dGuess:
+			if (button) {states = waitUpA;}
+			//states = dGuess;
+			break;
+		case waitUpA:
+			if (!button) {states = dAll;}
+			//states = waitUpA;
+			break;
+		default:
+			states = start;
+			break;
+	}
+	switch(states) {
+		case start:
+			break;
+		case dAll:
+			PORTD = 0x01;
+			index = 0;
+			lcd_setXY(0x40,0x80);
+		  	memset(buf, 0, 128);
+			strcpy(buf, allMode);
+			for (j=3; j<32; j++) {
+	   			index += sprintf(&buf[index], "%d ", specbuff[j]);
+			}
+			break;
+		case waitUpG:
+			memset(buf, 0, 128);
+			break;
+		case dGuess:
+			PORTD = 0x02;
+			index = 0;
+			lcd_setXY(0x40,0x80);
+		  	memset(buf, 0, 128);
+			//for (j=2; j<32; j++) {
+	   		//	index += sprintf(&buf[index], "%d ", specbuff[j]);
+			//}
+			l = 0;
+			m = 0;
+			h = 0;
+			for (j = 3; j < 13; j++) {
+				l+= specbuff[j];
+			}
+			for (j = 13; j < 23; j++) {
+				m+= specbuff[j];
+			}
+			for (j = 23; j < 32; j++) {
+				h+= specbuff[j];
+			}
+			
+			if (l < 1 && m < 1 && h < 1) { strcpy(buf, no);}
+			else if (l > m && l > h) { strcpy(buf, low);}
+			else if (m > l && m > h) { strcpy(buf, medium);}
+			else if (h > l && h > m) { strcpy(buf, high);}
+			break;
+		case waitUpA:
+			memset(buf, 0, 128);
+			break;
+		default:
+			break;
+	}
+}
  
 
 int main()
 {
+	DDRC = 0x00;
+	PORTC = 0xFF;
+	DDRD = 0xFF;
+	PORTD = 0x00;
 	SPI_Init();
 	N5110_init();
 	N5110_clear();
@@ -179,82 +276,65 @@ int main()
   	OCR1B = SLEEP_TIME;	// time to go to sleep
   	TIMSK1 = _BV(OCIE1B) | _BV(OCIE1A);
 
-	int x, y, z;
 	ADC_init();
 	adcind = 0;
 	currbin = 0;
 
 	//loop iterator
-	  int i;
-	  // generate arrays
-	  for (i=0; i<N_WAVE; i++) {
-	    // Set up FFT, one cycle sine table required for FFT
-	    Sinewave[i] = float2fix(sin(6.283*((float)i)/N_WAVE)); 
-		// generate empty array to erase
-		erasefi[i]=0;
-		// generate trapezoid mask (with 1/4 length slopes) for ADC buffer
-		if(i<32) adcMask[i] = float2fix((8*(float)i/255));
-		else if(i >= 32 && i <= 96) adcMask[i] = 0x0100;
-		else if(i > 96) adcMask[i] = float2fix(((128-(float)i)*8/255));
-	  }
-	  // generate empty array to erase
-	  for (i=0; i<spectrum_bins; i++)
-		erasespecbuff[i]=0;
+	int i;
+	// generate arrays
+	for (i=0; i<N_WAVE; i++) {
+	// Set up FFT, one cycle sine table required for FFT
+	Sinewave[i] = float2fix(sin(6.283*((float)i)/N_WAVE)); 
+	// generate empty array to erase
+	erasefi[i]=0;
+	// generate trapezoid mask (with 1/4 length slopes) for ADC buffer
+	if(i<32) adcMask[i] = float2fix((8*(float)i/255));
+	else if(i >= 32 && i <= 96) adcMask[i] = 0x0100;
+	else if(i > 96) adcMask[i] = float2fix(((128-(float)i)*8/255));
+	}
+	// generate empty array to erase
+	for (i=0; i<spectrum_bins; i++)
+	erasespecbuff[i]=0;
 
-	  // Set up single ADC timing with sleep mode
-	  sei();
-	  set_sleep_mode(SLEEP_MODE_IDLE);
-	  sleep_enable();
-	
+	// Set up single ADC timing with sleep mode
+	sei();
+	set_sleep_mode(SLEEP_MODE_IDLE);
+	sleep_enable();
+
 	int freqopt = 1;
 
 	int index = 0;
-	char buf[128];
-	int j;
-	int max;
 	
-	while (1) {	
-		if (1) {
-			// clear fft arrays
-			memcpy(specbuff,erasespecbuff,spectrum_bins);
-			memcpy(fi,erasefi,N_WAVE);
+	while (1) {
+		// clear fft arrays
+		memcpy(specbuff,erasespecbuff,spectrum_bins);
+		memcpy(fi,erasefi,N_WAVE);
 
-			// copy adc buffer into separate array
-			memcpy(fr, adcbuff, N_WAVE);
-			// scale  
-			for(i=0; i<N_WAVE; i++){
-				fr[i] = multfix((fr[i]<<4),adcMask[i]);
-			}
+		// copy adc buffer into separate array
+		memcpy(fr, adcbuff, N_WAVE);
+		// scale  
+		for(i=0; i<N_WAVE; i++){
+			fr[i] = multfix((fr[i]<<4),adcMask[i]);
+		}
+	
+		FFTfix(fr, fi, LOG2_N_WAVE);
+
+		for (i=0;i<(N_WAVE/2);i++) {
+			//Magnitude Function: Sum of Squares of the Real & Imaginary parts
+			fftarray[i]=multfix(fr[i],fr[i])+multfix(fi[i],fi[i]);
+			//store values into 32 frequency bins depending on overall frequency 
+			if (freqopt==0) specbuff[(char)(i/2)]+=(char)(fftarray[i]);
+			else if (freqopt==1 && i<32) specbuff[i]+=(char)(fftarray[i]);
+		} 
+		adcind = 0;
+		currbin = 0;
 		
-			FFTfix(fr, fi, LOG2_N_WAVE);
-
-			for (i=0;i<(N_WAVE/2);i++) {
-				//Magnitude Function: Sum of Squares of the Real & Imaginary parts
-				fftarray[i]=multfix(fr[i],fr[i])+multfix(fi[i],fi[i]);
-				//store values into 32 frequency bins depending on overall frequency 
-				if (freqopt==0) specbuff[(char)(i/2)]+=(char)(fftarray[i]);
-				else if (freqopt==1 && i<32) specbuff[i]+=(char)(fftarray[i]);
-			} 
-			adcind = 0;
-			currbin = 0;
-		}	
-		_delay_ms(50);
+		_delay_ms(100);
 		N5110_clear();
-		lcd_setXY(0x40,0x80);
-		index = 0;
-	  	memset(buf, 0, 128);
-		for (j=2; j<32; j++) {
-   			index += sprintf(&buf[index], "%d ", specbuff[j]);
-		}
-		N5110_Data(buf);
-		max = 0;
-		for (j=2; j<32; j++) {
-   			if(specbuff[j] > max) { max = j;}
-		}
-		if (max < 5) {max = 0;}
-		x = max;
-      		snprintf(buf, 40, "max: %d", x); // puts string into buffer
-		//N5110_Data(buf);
+
+		tick();
+		
 		N5110_Data(buf);				
 
 		
